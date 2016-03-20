9966cb185bc50b7697307658858c664850dd8dae
Usman-Sheikh <sh.usmanikram@hotmail.com>
2016-03-20 23:38:35 +0500

    Initial Commit

---
+++ eCommerce.DAL/Data/DataContext.cs
@@ -0,0 +1,32 @@
+﻿using eCommerce.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.DAL.Data
+{
+    public class DataContext : DbContext
+    {
+        /// <summary>
+        /// you can either pass the NAME of a conenction string (e.g. from a web.config), and explicitly delcare it.
+        /// </summary>
+        public DataContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        /// <summary>
+        /// any entity to be persisted must me delcared here.
+        /// </summary>
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
+
+    }
+}
---
+++ eCommerce.Model/BasketItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Model
+{
+    public class BasketItem
+    {
+        public int BasketItemId { get; set; }
+        public int BasketId { get; set; }
+        public int ProductId { get; set; }
+
+        public Product Product { get; set; }
+    }
+}
